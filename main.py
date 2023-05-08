db6f642f84509c21c9944be59be93c24f1a81748
alexandr <alsapozhnikov84@gmail.com>
2023-05-08 22:17:36 +0300

    home task

---
+++ main.py
@@ -0,0 +1,26 @@
+age = input('How old are you')
+age = int(age)
+if age >= 18:
+    print('it is fine')
+else:
+    print('it is not fine')
+age = input('give please name')
+age = int(age)
+if age > 100:
+    print('a lot')
+elif age > 18:
+    print('okey')
+else:
+    print('not okey')
+if age % 2:
+    print('even')
+else:
+    print('odd')
+name = input('What is your name?')
+a = 'a'
+if a in name:
+    print('not check')
+else:
+    print('check')
+
+
